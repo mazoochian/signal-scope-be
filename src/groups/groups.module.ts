07a1a24d3c29958aafca5242134f1e1196f24898
Armin Mazoochian <me@mazoochian.com>
2026-06-28 15:00:36 +0330

    Fix GroupsModule missing JwtService dependency
    Features Added:
     + Nothing
    Functionalities Revised:
     + Nothing
    Bugs Fixed:
     - Import UsersModule (which re-exports JwtModule) so JwtAuthGuard can resolve JwtService within GroupsModule context

--- src/groups/groups.module.ts
+++ src/groups/groups.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { GroupsController } from './groups.controller';
 import { GroupsService } from './groups.service';
 import { DbModule } from '../db/db.module';
+import { UsersModule } from '../users/users.module';
 
 @Module({
-  imports: [DbModule],
+  imports: [DbModule, UsersModule],
   controllers: [GroupsController],
   providers: [GroupsService],
 })
